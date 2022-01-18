b06ca9739f43cb6fb22fb72656330d7f17273b88
shino <shino.hinaduki@gmail.com>
2022-01-19 02:09:05 +0900

    現状仕様が曖昧な箇所を削除

--- src/main/scala/debug/VirtualCart.scala
+++ src/main/scala/debug/VirtualCart.scala
@@ -81,9 +81,9 @@ class NesFile(srcBytes: Array[Byte]) {
   val prgRomOffset  = trainerOffset + trainerSize
   val chrRomOffset  = prgRomOffset + prgRomSize
 
-  def TrainerBytes = if (trainerSize > 0) srcBytes.drop(trainerOffset).take(trainerSize) else Array.empty
-  def PrgRomBytes  = srcBytes.drop(prgRomOffset).take(prgRomSize)
-  def ChrRomBytes  = srcBytes.drop(chrRomOffset).take(chrRomSize)
+  def trainerBytes = if (trainerSize > 0) srcBytes.drop(trainerOffset).take(trainerSize) else Array.empty
+  def prgRomBytes  = srcBytes.drop(prgRomOffset).take(prgRomSize)
+  def chrRomBytes  = srcBytes.drop(chrRomOffset).take(chrRomSize)
 }
 
 /** Mapper機能をサポートしない、単純なカードリッジの模倣IP
@@ -92,75 +92,20 @@ class NesFile(srcBytes: Array[Byte]) {
   */
 class VirtualCart(initNesFilePath: String) extends Module {
   val io = IO(new Bundle {
-    val cpuBus = new CpuBus
-    val ppuBus = new PpuBus
-    val debugConfig = new Bundle {
-      val isWriteRom = Input(Bool()) // ROMへの書き込みを許可するならtrue
-    }
+    // TODO: 定義
   })
 
-  val ExpansionAreaRamBaseOffset = 0x4018;
-  val ExpansionAreaRamSize       = 0x2000 - 0x18;
-  val ExpansionAreaRam           = SyncReadMem(ExpansionAreaRamSize, UInt(8.W))
+  // val ExpansionAreaRamBaseOffset = 0x4018;
+  // val ExpansionAreaRamSize       = 0x2000 - 0x18;
+  // val ExpansionAreaRam           = SyncReadMem(ExpansionAreaRamSize, UInt(8.W))
 
-  val BatteryBackedRamBaseOffset = 0x6000;
-  val BatteryBackedRamSize       = 0x2000;
-  val BatteryBackedRam           = SyncReadMem(BatteryBackedRamSize, UInt(8.W))
+  // val BatteryBackedRamBaseOffset = 0x6000;
+  // val BatteryBackedRamSize       = 0x2000;
+  // val BatteryBackedRam           = SyncReadMem(BatteryBackedRamSize, UInt(8.W))
 
-  val PrgRomBaseOffset = 0x8000;
-  val PrgRomSize       = 0x8000;
-  val PrgRom           = SyncReadMem(PrgRomSize, UInt(8.W))
-
-  // CpuBus
-  // 0x0000 - 0x4017: Don't care
-  // 0x4018 - 0x5fff: Expansion RAM
-  // 0x6000 - 0x7fff: Battery-Backed RAM
-  // 0x8000 - 0xffff: PRG ROM
-  when(io.cpuBus.addr < BatteryBackedRamBaseOffset.U) {
-    io.cpuBus.dataOut := DontCare
-  }.elsewhen(io.cpuBus.addr < BatteryBackedRamBaseOffset.U) {
-    when(io.cpuBus.wen) {
-      ExpansionAreaRam.write(io.cpuBus.addr, io.cpuBus.dataIn)
-      io.cpuBus.dataOut := DontCare
-    }.otherwise {
-      io.cpuBus.dataOut := ExpansionAreaRam.read(io.cpuBus.addr)
-    }
-  }.elsewhen(io.cpuBus.addr < PrgRomBaseOffset.U) {
-    when(io.cpuBus.wen) {
-      BatteryBackedRam.write(io.cpuBus.addr, io.cpuBus.dataIn)
-      io.cpuBus.dataOut := DontCare
-    }.otherwise {
-      io.cpuBus.dataOut := BatteryBackedRam.read(io.cpuBus.addr)
-    }
-  }.otherwise {
-    when(io.cpuBus.wen) {
-      // ROMなので本来はWriteできない
-      when(io.debugConfig.isWriteRom) {
-        PrgRom.write(io.cpuBus.addr, io.cpuBus.dataIn)
-      }
-      io.cpuBus.dataOut := DontCare
-    }.otherwise {
-      io.cpuBus.dataOut := PrgRom.read(io.cpuBus.addr)
-    }
-  }
-
-  val ChrRomBaseOffset = 0x0000;
-  val ChrRomSize       = 0x2000;
-  val ChrRom           = SyncReadMem(ChrRomSize, UInt(8.W))
-
-  // PpuBus
-  // 0x0000_0000 - 0x0000_1fff: CHR ROM/RAM(Pattern Table 0/1)
-  // 0x0000_2000 - 0xffff_ffff: Don't Care
-  when(io.ppuBus.addr < ChrRomSize.U) {
-    when(io.ppuBus.wen) {
-      ChrRom.write(io.ppuBus.addr, io.ppuBus.dataIn)
-      io.ppuBus.dataOut := DontCare
-    }.otherwise {
-      io.ppuBus.dataOut := ChrRom.read(io.ppuBus.addr)
-    }
-  }.otherwise {
-    io.ppuBus.dataOut := DontCare
-  }
+  // val PrgRomBaseOffset = 0x8000;
+  // val PrgRomSize       = 0x8000;
+  // val PrgRom           = SyncReadMem(PrgRomSize, UInt(8.W))
 
   // .NES Fileを初期値としてロード
   val nesFile = new NesFile(initNesFilePath)
