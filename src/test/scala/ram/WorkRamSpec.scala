d4865efbe8e6a82bfc19a6ab9affc74a7831aa5f
shino <shino.hinaduki@gmail.com>
2022-01-19 02:45:51 +0900

    test: WorkRamのテスト項目を追加

--- src/test/scala/ram/WorkRamSpec.scala
+++ src/test/scala/ram/WorkRamSpec.scala
@@ -5,7 +5,6 @@ import chiseltest._
 import org.scalatest.freespec.AnyFreeSpec
 
 class WorkRamSpec extends AnyFreeSpec with ChiselScalatestTester {
-
   "Read and confirm what you have written" in {
     test(new WorkRam()) { dut =>
       // write incremental pattern
@@ -26,6 +25,78 @@ class WorkRamSpec extends AnyFreeSpec with ChiselScalatestTester {
         dut.io.nWriteEnable.poke(true.B)
         dut.clock.step(1)
         dut.io.dataOut.expect((i & 0xff).U)
+      }
+    }
+  }
+
+  "Chip is not selected" in {
+    test(new WorkRam()) { dut =>
+      // write incremental pattern
+      for (i <- 0 until 0x0800) {
+        dut.io.addr.poke(i.U)
+        dut.io.dataIn.poke((i & 0xff).U)
+        dut.io.nChipSelect.poke(true.B)
+        dut.io.nOutputEnable.poke(true.B)
+        dut.io.nWriteEnable.poke(false.B)
+        dut.clock.step(1)
+      }
+      // read incremental pattern
+      for (i <- 0 until 0x0800) {
+        dut.io.addr.poke(i.U)
+        dut.io.dataIn.poke(0.U)
+        dut.io.nChipSelect.poke(false.B)
+        dut.io.nOutputEnable.poke(false.B)
+        dut.io.nWriteEnable.poke(true.B)
+        dut.clock.step(1)
+        dut.io.dataOut.expect(0x00.U)
+      }
+    }
+  }
+
+  "Write is disabled" in {
+    test(new WorkRam()) { dut =>
+      // write incremental pattern
+      for (i <- 0 until 0x0800) {
+        dut.io.addr.poke(i.U)
+        dut.io.dataIn.poke((i & 0xff).U)
+        dut.io.nChipSelect.poke(false.B)
+        dut.io.nOutputEnable.poke(true.B)
+        dut.io.nWriteEnable.poke(true.B)
+        dut.clock.step(1)
+      }
+      // read incremental pattern
+      for (i <- 0 until 0x0800) {
+        dut.io.addr.poke(i.U)
+        dut.io.dataIn.poke(0.U)
+        dut.io.nChipSelect.poke(false.B)
+        dut.io.nOutputEnable.poke(false.B)
+        dut.io.nWriteEnable.poke(true.B)
+        dut.clock.step(1)
+        dut.io.dataOut.expect(0x00.U)
+      }
+    }
+  }
+
+  "Read is disabled" in {
+    test(new WorkRam()) { dut =>
+      // write incremental pattern
+      for (i <- 0 until 0x0800) {
+        dut.io.addr.poke(i.U)
+        dut.io.dataIn.poke((i & 0xff).U)
+        dut.io.nChipSelect.poke(false.B)
+        dut.io.nOutputEnable.poke(true.B)
+        dut.io.nWriteEnable.poke(false.B)
+        dut.clock.step(1)
+      }
+      // read incremental pattern
+      for (i <- 0 until 0x0800) {
+        dut.io.addr.poke(i.U)
+        dut.io.dataIn.poke(0.U)
+        dut.io.nChipSelect.poke(false.B)
+        dut.io.nOutputEnable.poke(true.B)
+        dut.io.nWriteEnable.poke(true.B)
+        dut.clock.step(1)
+        dut.io.dataOut.expect(0x00.U)
       }
     }
   }
