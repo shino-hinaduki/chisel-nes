7ba6af5189e4a421b27351a5c45c08ae7e1da087
shino <shino.hinaduki@gmail.com>
2022-01-20 02:04:00 +0900

    test: 飛び飛びにWriteするパターンを追加

--- src/test/scala/ram/WorkRamSpec.scala
+++ src/test/scala/ram/WorkRamSpec.scala
@@ -100,4 +100,37 @@ class WorkRamSpec extends AnyFreeSpec with ChiselScalatestTester {
       }
     }
   }
+
+  "Writing in stripes" in {
+    test(new WorkRam()) { dut =>
+      // write stripes pattern
+      for (i <- 0 until 0x0800) {
+        dut.io.addr.poke(i.U)
+        dut.io.dataIn.poke((i & 0xff).U)
+        dut.io.nChipSelect.poke((i % 2 == 0).B)
+        dut.io.nOutputEnable.poke(true.B)
+        dut.io.nWriteEnable.poke(false.B)
+        dut.clock.step(1)
+      }
+      for (i <- 0 until 0x0800) {
+        dut.io.addr.poke(i.U)
+        dut.io.dataIn.poke((0xff - (i & 0xff)).U)
+        dut.io.nChipSelect.poke(false.B)
+        dut.io.nOutputEnable.poke(true.B)
+        dut.io.nWriteEnable.poke((i % 2 == 1).B)
+        dut.clock.step(1)
+      }
+      // read decremental pattern
+      for (j <- 0 until 0x0800) {
+        val i = 0x0800 - j - 1
+        dut.io.addr.poke(i.U)
+        dut.io.dataIn.poke(0.U)
+        dut.io.nChipSelect.poke(false.B)
+        dut.io.nOutputEnable.poke(false.B)
+        dut.io.nWriteEnable.poke(true.B)
+        dut.clock.step(1)
+        dut.io.dataOut.expect(if (i % 2 == 1) (i & 0xff).U else (0xff - (i & 0xff)).U)
+      }
+    }
+  }
 }
