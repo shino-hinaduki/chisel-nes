f9aaff411f42495e93f6027ecd63b85b9e4df0ec
shino <shino.hinaduki@gmail.com>
2022-01-21 02:55:03 +0900

    feat: Decoder.scalaを追加

---
+++ src/main/scala/decoder/Decoder.scala
@@ -0,0 +1,43 @@
+package decoder
+
+import chisel3._
+import chisel3.util.switch
+import chisel3.util.Cat
+import chisel3.util.is
+
+/**
+  * LS139, CPU Busのアドレス線から /RAMSEL, /DBE, /ROMSEL 信号を生成する
+  * 
+  * Truth Table
+  * | EN | SEL   | OUTPUTS           |
+  * | /G | B | A | Y3 | Y2 | Y1 | Y0 |
+  * | H  | X | X | H  | H  | H  | H  |
+  * | L  | L | L | H  | H  | H  | L  |
+  * | L  | L | H | H  | H  | L  | H  |
+  * | L  | H | L | H  | L  | H  | H  |
+  * | L  | H | H | L  | H  | H  | H  |
+  * 
+  * { B, A }で選んだ数字がYxにデコードされて出てくるイメージ
+  */
+
+class Decoder extends Module {
+  val io = IO(new Bundle {
+    // select A
+    val a = Input(Bool())
+    // select B
+    val b = Input(Bool())
+    // Enable /G
+    val nG = Input(Bool())
+    // Outputs Y
+    val y = Output(UInt(4.W))
+  })
+  // select A/Bを数値化
+  val input = Cat(io.b, io.a)
+
+  when(io.nG) {
+    // /G=H
+    io.y := "b1111".U
+  }.otherwise {
+    // TODO:
+  }
+}
