acf73943025cca3dff7132a43e222be4fb2f7675
shino <shino.hinaduki@gmail.com>
2022-01-24 02:47:57 +0900

    test: パラレル入力のテストを実装

--- src/test/scala/joy/ShiftRegisterSpec.scala
+++ src/test/scala/joy/ShiftRegisterSpec.scala
@@ -5,22 +5,23 @@ import chiseltest._
 import org.scalatest.freespec.AnyFreeSpec
 
 class ShiftRegisterSpec extends AnyFreeSpec with ChiselScalatestTester {
+  val testDatas = Array(0x00, 0xff, 0xaa, 0x55, 0xa5, 0x5a, 0xab, 0xcd, 0xef)
+
   "Serial input test" in {
     test(new ShiftRegister) { dut =>
       {
-        val testDatas = Array(0x00, 0xff, 0xaa, 0x55, 0xa5, 0x5a)
         testDatas foreach { testData =>
           {
             // 8cycleかけてデータ入力
             for (i <- 0 until 8) {
               dut.io.pi.poke(0x00.U)
-              dut.io.si.poke((((testData >> i) & 0x1) == 0x1).B)
+              dut.io.si.poke((((testData >> (7 - i)) & 0x1) == 0x1).B)
               dut.io.isParallel.poke(false.B)
               dut.clock.step(1)
             }
-            // 現時点でtestDatas(0)が見えているはずなので今回1cyc+7cycでデータを確認
+            // 現時点でtestDatas(7)が見えているはずなので今回1cyc+7cycでデータを確認
             for (i <- 0 until 8) {
-              dut.io.q7.expect((((testData >> i) & 0x1) == 0x1).B)
+              dut.io.q7.expect((((testData >> (7 - i)) & 0x1) == 0x1).B)
               dut.io.isParallel.poke(false.B)
               dut.clock.step(1)
             }
@@ -29,4 +30,27 @@ class ShiftRegisterSpec extends AnyFreeSpec with ChiselScalatestTester {
       }
     }
   }
+
+  "Parallel input test" in {
+    test(new ShiftRegister) { dut =>
+      {
+        testDatas foreach { testData =>
+          {
+            // 1cycleかけてデータ入力
+            dut.io.pi.poke(testData.U)
+            dut.io.isParallel.poke(true.B)
+            dut.clock.step(1)
+
+            // 現時点でtestDatas(7)が見えているはずなので今回1cyc+7cycでデータを確認
+            for (i <- 0 until 8) {
+              dut.io.q7.expect((((testData >> (7 - i)) & 0x1) == 0x1).B)
+              dut.io.isParallel.poke(false.B)
+              dut.clock.step(1)
+            }
+          }
+        }
+      }
+    }
+  }
+
 }
