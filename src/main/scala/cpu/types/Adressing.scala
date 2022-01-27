81f7f3a85d1e19d48061bddf4888be0dfa943898
shino <shino.hinaduki@gmail.com>
2022-01-28 02:40:54 +0900

    feat: Addressing, Instructionの定義を追加

---
+++ src/main/scala/cpu/types/Adressing.scala
@@ -0,0 +1,22 @@
+package cpu.types
+
+import chisel3._
+import chisel3.experimental.ChiselEnum
+
+/** AddressingModeの定義
+  */
+object Addressing extends ChiselEnum {
+  val Implied     = Value
+  val Accumulator = Value
+  val Immediate   = Value
+  val Absolute    = Value
+  val ZeroPage    = Value
+  val ZeroPageX   = Value
+  val ZeroPageY   = Value
+  val AbsoluteX   = Value
+  val AbsoluteY   = Value
+  val Relative    = Value
+  val Indirect    = Value
+  val IndirectX   = Value
+  val IndirectY   = Value
+}
---
+++ src/main/scala/cpu/types/Instruction.scala
@@ -0,0 +1,33 @@
+package cpu.types
+
+import chisel3._
+import chisel3.experimental.ChiselEnum
+
+/** 命令の定義
+  */
+object Instruction extends ChiselEnum {
+  // binary op
+  val ADC, SBC, AND, EOR, ORA = Value
+  // shift/rotate
+  val ASL, LSR, ROL, ROR = Value
+  // inc/dec
+  val INC, INX, INY, DEC, DEX, DEY = Value
+  // load/store
+  val LDA, LDX, LDY, STA, STX, STY = Value
+  // set/clear
+  val SEC, SED, SEI, CLC, CLD, CLI, CLV = Value
+  // compare
+  val CMP, CPX, CPY = Value
+  // jump return
+  val JMP, JSR, RTI, RTS = Value
+  // branch
+  val BCC, BCS, BEQ, BMI, BNE, BPL, BVC, BVS = Value
+  // push/pop
+  val PHA, PHP, PLA, PLP = Value
+  // transfer
+  val TAX, TAY, TSX, TXA, TXS, TYA = Value
+  // other
+  val BRK, BIT, NOP = Value
+  // unofficial1
+  val ALR, ANC, ARR, AXS, LAX, SAX, DCP, ISC, RLA, RRA, SLO, SRE, SKB, IGN = Value
+}
