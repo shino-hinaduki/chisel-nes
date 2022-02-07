f32cc6c15660397f06890f562532388c47944531
shino <shino.hinaduki@gmail.com>
2022-02-08 02:44:55 +0900

    feat: PlatformDesignerでJtagDebug用デザイン作成

--- eda/quartusprime_lite_21.1/DE0_CV_golden_top.v
+++ eda/quartusprime_lite_21.1/DE0_CV_golden_top.v
@@ -156,25 +156,36 @@ module DE0_CV_golden_top (
 
 
 /*****************************************************************/
-// virtualjtag test
-//wire [23:0] vjtag_ir_in; // vjtag0.ir_in
-//wire vjtag_state_uir;    // Hのときにvjtag_ir_inを取り込む
-//virtual_jtag vjtag0 (
-//	  .tdi                (<connected-to-tdi>),                // jtag.tdi
-//	  .tdo                (<connected-to-tdo>),                //     .tdo
-//	  .ir_in              (vjtag_ir_in),                       //     .ir_in
-//	  .ir_out             (<connected-to-ir_out>),             //     .ir_out
-//	  .virtual_state_cdr  (<connected-to-virtual_state_cdr>),  //     .virtual_state_cdr
-//	  .virtual_state_sdr  (<connected-to-virtual_state_sdr>),  //     .virtual_state_sdr
-//   .virtual_state_e1dr (<connected-to-virtual_state_e1dr>), //     .virtual_state_e1dr
-//	  .virtual_state_pdr  (<connected-to-virtual_state_pdr>),  //     .virtual_state_pdr
-//	  .virtual_state_e2dr (<connected-to-virtual_state_e2dr>), //     .virtual_state_e2dr
-//	  .virtual_state_udr  (<connected-to-virtual_state_udr>),  //     .virtual_state_udr
-//	  .virtual_state_cir  (<connected-to-virtual_state_cir>),  //     .virtual_state_cir
-//	  .virtual_state_uir  (vjtag_state_uir),                   //     .virtual_state_uir
-//	  .tck                (<connected-to-tck>)                 //  tck.clk
+// for jtag debug
+//debug_subsystem debug_subsystem0 (
+    // .clk_clk                                                 (CLOCK_50CLOCK_50CLOCK_50),                                                 //                                         clk.clk
+    // .cpu_debug_bus_bridge_0_interrupt_irq                    (<connected-to-cpu_debug_bus_bridge_0_interrupt_irq>),                    //            cpu_debug_bus_bridge_0_interrupt.irq
+    // .ppu_debug_bus_bridge_0_interrupt_irq                    (<connected-to-ppu_debug_bus_bridge_0_interrupt_irq>),                    //            ppu_debug_bus_bridge_0_interrupt.irq
+    // .reset_reset_n                                           (RESET_NRESET_NRESET_N),                                           //                                       reset.reset_n
+    // .to_external_bus_bridge_0_external_interface_acknowledge (<connected-to-to_external_bus_bridge_0_external_interface_acknowledge>), // to_external_bus_bridge_0_external_interface.acknowledge
+    // .to_external_bus_bridge_0_external_interface_irq         (<connected-to-to_external_bus_bridge_0_external_interface_irq>),         //                                            .irq
+    // .to_external_bus_bridge_0_external_interface_address     (<connected-to-to_external_bus_bridge_0_external_interface_address>),     //                                            .address
+    // .to_external_bus_bridge_0_external_interface_bus_enable  (<connected-to-to_external_bus_bridge_0_external_interface_bus_enable>),  //                                            .bus_enable
+    // .to_external_bus_bridge_0_external_interface_byte_enable (<connected-to-to_external_bus_bridge_0_external_interface_byte_enable>), //                                            .byte_enable
+    // .to_external_bus_bridge_0_external_interface_rw          (<connected-to-to_external_bus_bridge_0_external_interface_rw>),          //                                            .rw
+    // .to_external_bus_bridge_0_external_interface_write_data  (<connected-to-to_external_bus_bridge_0_external_interface_write_data>),  //                                            .write_data
+    // .to_external_bus_bridge_0_external_interface_read_data   (<connected-to-to_external_bus_bridge_0_external_interface_read_data>),   //                                            .read_data
+    // .to_external_bus_bridge_1_external_interface_acknowledge (<connected-to-to_external_bus_bridge_1_external_interface_acknowledge>), // to_external_bus_bridge_1_external_interface.acknowledge
+    // .to_external_bus_bridge_1_external_interface_irq         (<connected-to-to_external_bus_bridge_1_external_interface_irq>),         //                                            .irq
+    // .to_external_bus_bridge_1_external_interface_address     (<connected-to-to_external_bus_bridge_1_external_interface_address>),     //                                            .address
+    // .to_external_bus_bridge_1_external_interface_bus_enable  (<connected-to-to_external_bus_bridge_1_external_interface_bus_enable>),  //                                            .bus_enable
+    // .to_external_bus_bridge_1_external_interface_byte_enable (<connected-to-to_external_bus_bridge_1_external_interface_byte_enable>), //                                            .byte_enable
+    // .to_external_bus_bridge_1_external_interface_rw          (<connected-to-to_external_bus_bridge_1_external_interface_rw>),          //                                            .rw
+    // .to_external_bus_bridge_1_external_interface_write_data  (<connected-to-to_external_bus_bridge_1_external_interface_write_data>),  //                                            .write_data
+    // .to_external_bus_bridge_1_external_interface_read_data   (<connected-to-to_external_bus_bridge_1_external_interface_read_data>),   //                                            .read_data
+    // .clk_1_clk_clk                                           (<connected-to-clk_1_clk_clk>),                                           //                                   clk_1_clk.clk
+    // .clk_1_clk_reset_reset_n                                 (<connected-to-clk_1_clk_reset_reset_n>),                                 //                             clk_1_clk_reset.reset_n
+    // .clk_ppu_0_clk_clk                                       (<connected-to-clk_ppu_0_clk_clk>),                                       //                               clk_ppu_0_clk.clk
+    // .clk_ppu_0_clk_reset_reset_n                             (<connected-to-clk_ppu_0_clk_reset_reset_n>)                              //                         clk_ppu_0_clk_reset.reset_n
 // );
 
+
+/*****************************************************************/
 // TODO: inout兼用ピンが未定義のwireのまま
 ChiselNes chiselNes0(
 //  .clock(clock),
