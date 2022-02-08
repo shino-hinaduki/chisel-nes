f3f7c5a5cd2a607bb719a805093e40a671162667
shino <shino.hinaduki@gmail.com>
2022-02-09 03:27:30 +0900

    wip: OF実装中

--- src/main/scala/cpu/execute/OperandFetch.scala
+++ src/main/scala/cpu/execute/OperandFetch.scala
@@ -5,11 +5,13 @@ import chisel3.experimental.ChiselEnum
 
 import bus.BusSlavePort
 import cpu.types.Addressing
+import chisel3.util.switch
+import chisel3.util.is
 
 /** OperandFetch状況を示します
  */
 object OperandFetchStatus extends ChiselEnum {
-  val idle, read = Value
+  val idle, readRom, readRam = Value
 }
 
 /** OperandFetchする機能を提供する, 使う側はFlippedして使う
@@ -19,27 +21,29 @@ class OperandFetchControl extends Bundle {
   val reqStrobe = Input(Bool())
   // 命令が配置されていたアドレス
   val opcodeAddr = Input(UInt(16.W))
-  // Fetchした結果を破棄する場合はtrue
-  val discard = Input(Bool())
   // Decodeした命令のアドレッシング方式
   val addressing = Input(Addressing())
   // アドレスを求めるだけであればfalse(メモリ転送系の命令など)、それ以外はtrue
-  val reqReadData = Input(Bool())
+  val reqDataFetch = Input(Bool())
+  // A reg。そのまま見せれば良い
+  val a = Input(UInt(8.W))
   // X reg。そのまま見せれば良い
-  val regX = Input(UInt(8.W))
+  val x = Input(UInt(8.W))
   // Y reg。そのまま見せれば良い
-  val regY = Input(UInt(8.W))
+  val y = Input(UInt(8.W))
 
   // 処理中であればtrue, この状態ではreqStrobeを受け付けない
   val busy = Output(Bool())
-  // 処理完了後、有効なデータになっていればtrue
+  // 処理完了後、有効なデータになっていればtrue。reqStrobeから最短1cycで出力
   val valid = Output(Bool())
   // 対象のアドレス
   val dstAddr = Output(UInt(16.W))
   // 読みだした結果。reqReadDataがfalseの場合はDon't care
   val readData = Output(UInt(16.W))
-  // A regを参照してほしい場合はtrue(そもそもOperandFetchに投げる必要ない)
-  val referA = Output(Bool())
+  // dstAddrに有効なデータが入っていればtrue. A regを参照してほしい場合はfalse
+  val dstAddrValid = Output(Bool())
+  // readDataに有効なデータが入っていればtrue. Implied, Accumulate, MemWrite系の命令だとfalse
+  val readDataValid = Output(Bool())
 }
 
 /** 指定されたAddressing modeに従ってデータを読み出します
@@ -55,33 +59,128 @@ class OperandFetch extends Module {
   })
 
   // 内部
-  val statusReg = RegInit(OperandFetchStatus(), OperandFetchStatus.idle)
-  val readCount = RegInit(UInt(3.W), 0.U) // 最大でIndirectIndexedでデータを読み出すケースで4回
+  val statusReg        = RegInit(OperandFetchStatus(), OperandFetchStatus.idle)
+  val currentReadCount = RegInit(UInt(3.W), 0.U) // 最大でIndirectIndexedでデータを読み出すケースで4回
+  val totalReadCount   = RegInit(UInt(3.W), 0.U) // 最大でIndirectIndexedでデータを読み出すケースで4回
   // 制御入力
   val prevReqStrobeReg = RegInit(Bool(), false.B)
   // BusMaster関連
+  val reqReadReg     = RegInit(Bool(), false.B)
   val readReqAddrReg = RegInit(UInt(16.W), 0.U)
   // 結果出力
-  val validReg    = RegInit(Bool(), false.B)
-  val dstAddrReg  = RegInit(UInt(16.W), 0.U)
-  val readDataReg = RegInit(UInt(16.W), 0.U)
-  val referAReg   = RegInit(Bool(), false.B)
+  val validReg         = RegInit(Bool(), false.B)
+  val dstAddrReg       = RegInit(UInt(16.W), 0.U)
+  val readDataReg      = RegInit(UInt(16.W), 0.U)
+  val dstAddrValidReg  = RegInit(Bool(), false.B)
+  val readDataValidReg = RegInit(Bool(), false.B)
 
   // internal
   io.status := statusReg
   // BusMaster -> BusArbiterSlavePort
   io.busMaster.addr        := readReqAddrReg
-  io.busMaster.req         := statusReg === OperandFetchStatus.read // status=Readであれば処理し続ける
-  io.busMaster.writeEnable := false.B                               // Read Only
-  io.busMaster.dataIn      := DontCare                              // Writeすることはない
+  io.busMaster.req         := reqReadReg
+  io.busMaster.writeEnable := false.B  // Read Only
+  io.busMaster.dataIn      := DontCare // Writeすることはない
   // EXにはそのまま結果を見せる
-  io.control.busy     := statusReg === OperandFetchStatus.read
-  io.control.valid    := validReg
-  io.control.dstAddr  := dstAddrReg
-  io.control.readData := readDataReg
-  io.control.referA   := referAReg
+  io.control.busy          := statusReg =/= OperandFetchStatus.idle
+  io.control.valid         := validReg
+  io.control.dstAddr       := dstAddrReg
+  io.control.readData      := readDataReg
+  io.control.dstAddrValid  := dstAddrValidReg
+  io.control.readDataValid := readDataValidReg
 
   // Req立ち上がり検出用
   val onRequest = (!prevReqStrobeReg) & io.control.reqStrobe // 今回の立ち上がりで判断させる
   prevReqStrobeReg := io.control.reqStrobe
+
+  switch(statusReg) {
+    is(OperandFetchStatus.idle) {
+      // idle->read開始
+      when(onRequest) {
+        switch(io.control.addressing) {
+          // 処理不要
+          is(Addressing.implied) {
+            statusReg := OperandFetchStatus.idle // 完了
+
+            validReg         := true.B
+            dstAddrReg       := DontCare
+            readDataReg      := DontCare
+            dstAddrValidReg  := false.B
+            readDataValidReg := false.B
+
+            reqReadReg       := false.B
+            readReqAddrReg   := 0.U
+            currentReadCount := 0.U
+            totalReadCount   := 0.U
+          }
+          // データだけ載せて完了
+          is(Addressing.accumulator) {
+            statusReg := OperandFetchStatus.idle // 完了
+
+            validReg         := true.B
+            dstAddrReg       := DontCare
+            readDataReg      := io.control.a
+            dstAddrValidReg  := false.B
+            readDataValidReg := true.B
+
+            reqReadReg       := false.B
+            readReqAddrReg   := 0.U
+            currentReadCount := 0.U
+            totalReadCount   := 0.U
+          }
+          // 1dataだけ読んで終わり
+          is(Addressing.immediate) {
+            statusReg := OperandFetchStatus.readRom
+
+            validReg         := false.B
+            dstAddrReg       := 0.U
+            readDataReg      := 0.U
+            dstAddrValidReg  := false.B
+            readDataValidReg := false.B
+
+            reqReadReg       := true.B
+            readReqAddrReg   := (io.control.opcodeAddr + 1.U)
+            currentReadCount := 1.U
+            totalReadCount   := 1.U
+          }
+          // lower,upper
+          is(Addressing.absolute) {
+            statusReg := OperandFetchStatus.readRom
+
+            validReg         := false.B
+            dstAddrReg       := 0.U
+            readDataReg      := 0.U
+            dstAddrValidReg  := false.B
+            readDataValidReg := false.B
+
+            reqReadReg       := true.B
+            readReqAddrReg   := (io.control.opcodeAddr + 1.U)
+            currentReadCount := 1.U
+            totalReadCount   := 2.U
+          }
+          // TODO: 他のアドレッシングモード
+        }
+      }.otherwise {
+        // 現状維持
+        statusReg := OperandFetchStatus.idle
+
+        validReg         := validReg
+        dstAddrReg       := dstAddrReg
+        readDataReg      := readDataReg
+        dstAddrValidReg  := dstAddrValidReg
+        readDataValidReg := readDataValidReg
+
+        reqReadReg       := false.B
+        readReqAddrReg   := 0.U
+        currentReadCount := 0.U
+        totalReadCount   := 0.U
+      }
+    }
+    is(OperandFetchStatus.readRom) {
+      //TODO: totalReadCountまでよむ + reqDataFetch=trueなら継続してReadを仕掛ける
+    }
+    is(OperandFetchStatus.readRam) {
+      //TODO: totalReadCountまで回収して完了する
+    }
+  }
 }
