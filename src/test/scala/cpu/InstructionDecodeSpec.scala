587861c2172e894da303bc51cb556b74b3251b05
shino <shino.hinaduki@gmail.com>
2022-02-13 22:02:46 +0900

    test: ID test 0x00 ~ 0x0fを追加

--- src/test/scala/cpu/InstructionDecodeSpec.scala
+++ src/test/scala/cpu/InstructionDecodeSpec.scala
@@ -9,7 +9,22 @@ import cpu.types.Addressing
 class InstructionDecodeSpec extends AnyFreeSpec {
   // (別の表を見ながら順に作り直した...)
   val expectTable: Seq[(Int, (Instruction.Type, Addressing.Type))] = Seq(
-    0x00 -> (Instruction.brk, Addressing.implied)
+    0x00 -> (Instruction.brk, Addressing.implied),
+    0x01 -> (Instruction.ora, Addressing.xIndexedIndirect),
+    0x02 -> (Instruction.jam, Addressing.invalid),
+    0x03 -> (Instruction.slo, Addressing.xIndexedIndirect),
+    0x04 -> (Instruction.nop, Addressing.zeroPage),
+    0x05 -> (Instruction.ora, Addressing.zeroPage),
+    0x06 -> (Instruction.asl, Addressing.zeroPage),
+    0x07 -> (Instruction.slo, Addressing.zeroPage),
+    0x08 -> (Instruction.php, Addressing.implied),
+    0x09 -> (Instruction.ora, Addressing.immediate),
+    0x0a -> (Instruction.asl, Addressing.accumulator),
+    0x0b -> (Instruction.anc, Addressing.immediate),
+    0x0c -> (Instruction.nop, Addressing.absolute),
+    0x0d -> (Instruction.ora, Addressing.absolute),
+    0x0e -> (Instruction.asl, Addressing.absolute),
+    0x0f -> (Instruction.slo, Addressing.absolute),
   )
   // 本家で定義されているテーブル(UIntでMapにすると、論理環境でうまくKeyが見つからなかったので数値に変換)
   val decode: Map[Int, (Instruction.Type, Addressing.Type)] =
