cf8cd938f82e370228513a936cb753118e460dd5
shino <shino.hinaduki@gmail.com>
2022-03-11 03:57:23 +0900

    feat: IDCODE読み取りを動作確認できた

--- utils/ChiselNesViewer/ChiselNesViewer.Core.Test/Jtag/JtagMasterTest.cs
+++ utils/ChiselNesViewer/ChiselNesViewer.Core.Test/Jtag/JtagMasterTest.cs
@@ -4,8 +4,9 @@ using ChiselNesViewer.Core.Jtag;
 using ChiselNesViewer.Core.Jtag.Command;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace ChiselNesViewer.Core.Test {
+namespace ChiselNesViewer.Core.Test.Jtag {
     [TestClass]
+    [DoNotParallelize]
     public class JtagMasterTest {
         readonly string DeviceDescription = "USB-Blaster";
 
@@ -13,6 +14,7 @@ namespace ChiselNesViewer.Core.Test {
         /// デバイスの検索と接続
         /// </summary>
         [TestMethod]
+        [DoNotParallelize]
         public void Connect() {
             var jtag = new JtagMaster();
             var devices = JtagMaster.GetDevices();
@@ -23,9 +25,10 @@ namespace ChiselNesViewer.Core.Test {
         }
 
         /// <summary>
-        /// デバイスの検索と接続
+        /// IDCODEがDE0-CVに乗っているCyclone V E A4に一致しているか確認
         /// </summary>
         [TestMethod]
+        [DoNotParallelize]
         public void ReadIdcode() {
             var jtag = new JtagMaster();
             var devices = JtagMaster.GetDevices();
@@ -34,7 +37,11 @@ namespace ChiselNesViewer.Core.Test {
             var idcode = Idcode.Read(jtag);
             Assert.IsTrue(jtag.Close());
 
-            Console.WriteLine(idcode);
+            // Cyclone V E A4
+            //https://www.intel.co.jp/content/dam/altera-www/global/ja_JP/pdfs/literature/hb/cyclone-v/cv_52009_j.pdf
+            Assert.AreEqual(idcode.Version, 0b0000);
+            Assert.AreEqual(idcode.PartNumber, 0b0010_1011_0000_0101);
+            Assert.AreEqual(idcode.MakerId, 0b000_0110_1110);
         }
     }
 }
