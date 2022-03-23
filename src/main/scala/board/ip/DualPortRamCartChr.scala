c492319ad88fd3a68e52190da4fc57c9f2712b24
shino <shino.hinaduki@gmail.com>
2022-03-24 01:58:59 +0900

    feat: DPRAM関連の定義を追加

---
+++ src/main/scala/board/ip/DualPortRamCartChr.scala
@@ -0,0 +1,23 @@
+package board.ip
+
+import chisel3._
+
+/**
+  * dpram_cart_chr.v を chiselで取り扱うために用意した定義
+  */
+class dpram_cart_chr extends BlackBox {
+  val io = IO(new Bundle {
+    val address_a = Input(UInt(17.W)) // input	[16:0]  address_a;
+    val address_b = Input(UInt(17.W)) // input	[16:0]  address_b;
+    val clock_a   = Input(Clock())    // input	  clock_a;
+    val clock_b   = Input(Clock())    // input	  clock_b;
+    val data_a    = Input(UInt(8.W))  // input	[7:0]  data_a;
+    val data_b    = Input(UInt(8.W))  // input	[7:0]  data_b;
+    val rden_a    = Input(Bool())     // input	  rden_a;
+    val rden_b    = Input(Bool())     // input	  rden_b;
+    val wren_a    = Input(Bool())     // input	  wren_a;
+    val wren_b    = Input(Bool())     // input	  wren_b;
+    val q_a       = Output(UInt(8.W)) // output	[7:0]  q_a;
+    val q_b       = Output(UInt(8.W)) // output	[7:0]  q_b;
+  })
+}
---
+++ src/main/scala/board/ip/DualPortRamFrameBuffer.scala
@@ -0,0 +1,23 @@
+package board.ip
+
+import chisel3._
+
+/**
+  * dpram_framebuffer.v を chiselで取り扱うために用意した定義
+  */
+class dpram_framebuffer extends BlackBox {
+  val io = IO(new Bundle {
+    val address_a = Input(UInt(18.W))  // input	[17:0]  address_a;
+    val address_b = Input(UInt(18.W))  // input	[17:0]  address_b;
+    val clock_a   = Input(Clock())     // input	  clock_a;
+    val clock_b   = Input(Clock())     // input	  clock_b;
+    val data_a    = Input(UInt(24.W))  // input	[23:0]  data_a;
+    val data_b    = Input(UInt(24.W))  // input	[23:0]  data_b;
+    val rden_a    = Input(Bool())      // input	  rden_a;
+    val rden_b    = Input(Bool())      // input	  rden_b;
+    val wren_a    = Input(Bool())      // input	  wren_a;
+    val wren_b    = Input(Bool())      // input	  wren_b;
+    val q_a       = Output(UInt(24.W)) // output	[23:0]  q_a;
+    val q_b       = Output(UInt(24.W)) // output	[23:0]  q_b;
+  })
+}
