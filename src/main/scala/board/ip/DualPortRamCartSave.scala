03d2a19875a43cac878aa079e2927a4946797abe
shino <shino.hinaduki@gmail.com>
2022-04-08 03:52:43 +0900

    feat: battery-backupしているRAM用のIPを追加

---
+++ src/main/scala/board/ip/DualPortRamCartSave.scala
@@ -0,0 +1,25 @@
+package board.ip
+
+import chisel3._
+
+/**
+  * dpram_cart_save.v を chiselで取り扱うために用意した定義
+  */
+class DualPortRamCartSave extends BlackBox {
+  override def desiredName: String = "dpram_cart_save"
+
+  val io = IO(new Bundle {
+    val address_a = Input(UInt(13.W))  // input	[12:0]  address_a;
+    val address_b = Input(UInt(11.W))  // input	[10:0]  address_b;
+    val clock_a   = Input(Clock())     // input	  clock_a;
+    val clock_b   = Input(Clock())     // input	  clock_b;
+    val data_a    = Input(UInt(8.W))   // input	[7:0]  data_a;
+    val data_b    = Input(UInt(32.W))  // input	[31:0]  data_b;
+    val rden_a    = Input(Bool())      // input	  rden_a;
+    val rden_b    = Input(Bool())      // input	  rden_b;
+    val wren_a    = Input(Bool())      // input	  wren_a;
+    val wren_b    = Input(Bool())      // input	  wren_b;
+    val q_a       = Output(UInt(8.W))  // output	[7:0]  q_a;
+    val q_b       = Output(UInt(32.W)) // output	[31:0]  q_b;
+  })
+}
